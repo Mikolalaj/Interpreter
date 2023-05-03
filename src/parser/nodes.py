a931afd1f05f647f697d65129c537c0cc29a925b
Mikolaj Olejnik <olejnikmikolaj@gmail.com>
2023-05-03 23:00:32 +0200

    add more lexer stuff

--- src/parser/nodes.py
+++ src/parser/nodes.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Any, List
 
 
 class Parameter:
@@ -27,5 +27,50 @@ class FunctionDefinition:
     def __eq__(self, __value: object) -> bool:
         if isinstance(__value, FunctionDefinition):
             return self.name == __value.name and self.parameters == __value.parameters and self.body == __value.body
+        else:
+            return False
+
+
+class Assignment:
+    def __init__(self, name: str, value: Any):
+        self.name = name
+        self.value = value
+
+    def __repr__(self):
+        return f"(Argument:{self.name} Value:{self.value})"
+
+    def __eq__(self, __value: object) -> bool:
+        if isinstance(__value, Assignment):
+            return self.name == __value.name and self.value == __value.value
+        else:
+            return False
+
+
+class FunctionCall:
+    def __init__(self, name: str, arguments: List[Assignment]):
+        self.name = name
+        self.arguments = arguments
+
+    def __repr__(self):
+        return f"(FunctionCall:{self.name} Args:{self.arguments})"
+
+    def __eq__(self, __value: object) -> bool:
+        if isinstance(__value, FunctionCall):
+            return self.name == __value.name and self.arguments == __value.arguments
+        else:
+            return False
+
+
+class IfStatement:
+    def __init__(self, condition, body):
+        self.condition = condition
+        self.body = body
+
+    def __repr__(self):
+        return f"(If:{self.condition} Body:{self.body})"
+
+    def __eq__(self, __value: object) -> bool:
+        if isinstance(__value, IfStatement):
+            return self.condition == __value.condition and self.body == __value.body
         else:
             return False
